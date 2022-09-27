2fc71f8a807cb902722ed694d0763b9703076981
PV <patrick.voigt.i15b@nordakademie.org>
2022-09-27 12:53:26 +0200

    TECH: adds stock images, adds bait background

--- MMM-Halloween.js
+++ MMM-Halloween.js
@@ -4,76 +4,100 @@
  * By Voigtus
  * MIT Licensed.
  */
+let currentScareId = "";
+let baitId = "";
+let elements = 0;
 Module.register("MMM-Halloween", {
-    defaults: {
-        imageNameList: [],
-        videoNameList: [],
-        scareInterval: 0,
-        currentScareId: "",
-        scareTime: 0
-    },
+  defaults: {
+    imageNameList: [],
+    videoNameList: [],
+    scareInterval: 0,
+    scareTime: 0,
+    muted: true,
+    bait: ""
+  },
 
-    start: function () {
-        this.url = "";
-        setInterval(() => {
-            this.triggerNewScare();
-            setTimeout(() => {
-                this.hideScare();
-            }, this.config.scareTime);
-        }, this.config.scareInterval);
-    },
+  //set interval for scare
+  start: function () {
+    setInterval(() => {
+      triggerNewScare();
+      setTimeout(() => {
+        hideScare();
+      }, this.config.scareTime);
+    }, this.config.scareInterval);
+  },
 
-    // Override dom generator.
-    getDom: function () {
-        var wrapper = document.createElement("div");
-        var counter = 0;
-        this.config.videoNameList.forEach(v => {
-            var video = document.createElement('video');
-            video.src = `modules/halloween/videos/${v}`;
-            video.type = "video/mp4";
-            video.autoplay = false;
-            video.id = `scare_${counter}`;
-            video.hidden = true;
-            wrapper.appendChild(video);
-            counter++;
-        });
-        this.config.imageNameList.forEach(i => {
-            var image = document.createElement("img");
-            image.src = `modules/halloween/images/${i}`;
-            image.id = `scare_${counter}`;
-            image.hidden = true;
-            wrapper.appendChild(image);
-            counter++;
-        });
-        this.config.elements = counter;
-        return wrapper;
-    },
-
-    //TODO add reaction for motion sensor
-    notificationReceived(notification, payload, sender) {
-        if (notification === "MOTION_DETECTED") {
-            this.triggerNewScare();
-        }
-    },
-
-    triggerNewScare(){
-        const randomNumber = Math.floor(Math.random() * this.config.elements);
-        var newScare = document.getElementById(`scare_${randomNumber}`);
-        if( newScare.tagName == "VIDEO"){
-            newScare.currentTime = 0;
-            newScare.play();
-        }
-        newScare.hidden = false;
-        this.config.currentScareId = `scare_${randomNumber}`;
-    },
-
-    hideScare(){
-        if (this.config.currentScareId != ""){
-            var oldScare = document.getElementById(this.config.currentScareId);
-            if( oldScare.tagName == "VIDEO"){
-                oldScare.pause();
-            }
-            oldScare.hidden = true;
-        }
+  // Override dom generator.
+  getDom: function () {
+    var wrapper = document.createElement("div");
+    if (this.config.bait !== "") {
+      var image = document.createElement("img");
+      image.src = `modules/MMM-Halloween/bait/${this.config.bait}`;
+      image.id = `bait`;
+      image.hidden = false;
+      wrapper.appendChild(image);
+      baitId = `bait`;
     }
+
+    var counter = 0;
+    this.config.videoNameList.forEach(v => {
+      var video = document.createElement('video');
+      video.src = `modules/MMM-Halloween/videos/${v}`;
+      video.type = "video/mp4";
+      video.autoplay = false;
+      video.id = `scare_${counter}`;
+      video.hidden = true;
+      video.muted = this.muted;
+      wrapper.appendChild(video);
+      counter++;
+    });
+
+    this.config.imageNameList.forEach(imageName => {
+      var image = document.createElement("img");
+      image.src = `modules/MMM-Halloween/images/${imageName}`;
+      image.id = `scare_${counter}`;
+      image.hidden = true;
+      wrapper.appendChild(image);
+      counter++;
+    });
+
+    elements = counter;
+    return wrapper;
+  },
+
+  //TODO add reaction for motion sensor
+  // notificationReceived: function (notification, payload, sender) {
+  //   if (notification === "MOTION_DETECTED") {
+  //    triggerNewScare();
+  //   }
+  // },
 });
+
+function triggerNewScare() {
+  const randomNumber = Math.floor(Math.random() * elements);
+  var newScare = document.getElementById(`scare_${randomNumber}`);
+  if (newScare.tagName === "VIDEO") {
+    newScare.currentTime = 0;
+    newScare.play();
+  }
+  if(baitId!== ""){
+    var bait = document.getElementById(baitId);
+    bait.hidden = true;
+  }
+  newScare.hidden = false;
+  currentScareId = `scare_${randomNumber}`;
+}
+
+function hideScare() {
+  if (currentScareId !== "") {
+    var oldScare = document.getElementById(currentScareId);
+    if (oldScare.tagName === "VIDEO") {
+      oldScare.pause();
+    }
+    oldScare.hidden = true;
+  }
+  if(baitId!== ""){
+    var bait = document.getElementById(baitId);
+    bait.hidden = false;
+  }
+}
--- README.md
+++ README.md
@@ -4,12 +4,14 @@ This project started as a way to having a laugh (and some jumpscares) at hallowe
 
 Since no module for the [MagicMirror](https://docs.magicmirror.builders/) could fulfill the need I decided to make a new one.
 
+To not scare anyone without necessity I added some stock images, which are pretty wholesome. So you can give it a spin, even with kids or easily scared people around.
+
 ## Disclaimer
 
 Since this is my first MagicMirror module and the first time I did some JS in a long time, feel free to give some hints, what I could do better.
 And feel free to improve it or send some issues or ideas my way. This module can only get better.
 
-What bugs me the most is to create all videos and images prior and "unhide/hide" them at an instant. I failed to get them to play by just updating the DOM. 
+What bugs me the most is to create all videos and images prior and "unhide/hide" them at an instant. I failed to get them to play by just updating the DOM.
 If anyone has an idea, feel free to fix that or shoot a DM my way.
 
 ## Installation
@@ -22,7 +24,7 @@ Clone the repo in your /modules folder of your magic mirror.
 git clone git@github.com:Voigtus/MMM-Halloween.git
 ```
 ### Sound
-If you are using a modern browser, you will realise, that no sound is available. This is due to a policy in most browsers to stop the 
+If you are using a modern browser, you will realise, that no sound is available. This is due to a policy in most browsers to stop the
 epidemic of annoying videos who started to blast in your face the instant you opened a page. [See here for chrome for example](https://developer.chrome.com/blog/autoplay/)
 
 To enable it in Brave follow these steps:
@@ -37,17 +39,26 @@ This might differ to Chrome or Firefox, so feel free to add this.
 ### Config parameters
 There are some parameters you have to overwrite in your config.js. For more information regarding the config.js see the [official documentation](https://docs.magicmirror.builders/modules/configuration.html)
 
-| parameter      | default | explanation                                                                |
-|----------------|---------|----------------------------------------------------------------------------|
-| imageNameList  | []      | An array containing all names of your images you want to scare people with |
-| videoNameList  | []      | An array containing all names of your videos you want to scare people with |
-| scareInterval  | 0       | Interval, when a scare should happen. In ms                                |
-| scareTime      | 0       | How long the scare should be visible on your mirror                        |
+| parameter     | default | explanation                                                                |
+|---------------|---------|----------------------------------------------------------------------------|
+| imageNameList | []      | An array containing all names of your images you want to scare people with |
+| videoNameList | []      | An array containing all names of your videos you want to scare people with |
+| scareInterval | 0       | Interval, when a scare should happen. In ms                                |
+| scareTime     | 0       | How long the scare should be visible on your mirror                        |
+| muted         | true    | Weather the videos you play should be muted                                |
+| bait          | ""        | Your bait, so people start to be interested.                               |
+
+
+## Stock images
+
+Added images and videos provided by pixabay under [Pixabay license](https://pixabay.com/de/service/license/)
+scare.jpg => https://pixabay.com/de/photos/margerite-g%c3%a4nsebl%c3%bcmchen-blume-wei%c3%9f-729510/
+scare_video.jpg => https://pixabay.com/de/videos/rock-balance-gestapelte-felsen-42470/
+bait.jpg => https://pixabay.com/de/photos/pendel-karte-navigation-kompass-1934311/
 
 ## Todos
 - Add Motion sensor support
-- Add possibility to mute/unmute videos by config
 - Add Sound installation for Chrome, Firefox, Edge
-- Add a "fishing background" to lure people in
 - Add a docker image
 - Add electron support
+- CSS for video and images, so they are centered and fullscreen
